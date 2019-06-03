ad63a34e186878a3864f9119b5a5cb878c625954
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2019-06-03 14:29:45 -0300

    Terminado a janela base

--- src/barestoque/view/Janela.java
+++ src/barestoque/view/Janela.java
@@ -4,47 +4,30 @@ import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 
 public class Janela extends JFrame
 {
-    private Dimension tamanhoDaTela;
+//    private Dimension tamanhoDaTela;
     private GraphicsDevice dispositivoGrafico;
     
-    private boolean telaCheia;
-    
-    public Janela (String titulo, int largura, int altura)
+    public Janela (String titulo)
     {
         super (titulo);
         
-        tamanhoDaTela = Toolkit.getDefaultToolkit().getScreenSize();
-        int larguraDaTela = tamanhoDaTela.width;
-        int alturaDaTela = tamanhoDaTela.height;
-        
-        dispositivoGrafico = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        
-        setSize (largura, altura);
+//        tamanhoDaTela = Toolkit.getDefaultToolkit().getScreenSize();
+//        int larguraDaTela = tamanhoDaTela.width;
+//        int alturaDaTela = tamanhoDaTela.height;
+
+        setUndecorated (true);
+        setBackground (Color.white);
         setDefaultCloseOperation (EXIT_ON_CLOSE);
         setVisible (true);
-        setLocation ((larguraDaTela - largura)/2, (alturaDaTela - altura)/2);
+        
+        dispositivoGrafico = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        dispositivoGrafico.setFullScreenWindow(this);
+        
     }
-    
-    public void setTelaCheia (boolean telaCheia)
-    {
-        //setVisible (false);
-        setUndecorated (telaCheia);
-        if (telaCheia)
-            dispositivoGrafico.setFullScreenWindow(this);
-        else 
-            dispositivoGrafico.setFullScreenWindow(null);
-        //setVisible (true);
-        this.telaCheia = telaCheia;
-    }
-    
-    public boolean estaEmTelaCheia ()
-    {
-        return telaCheia;
-    }
-    
 }

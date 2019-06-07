83629703cd32b76dd818feb3f3e7944bf620a8ab
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2019-06-07 16:09:29 -0300

    Adicionado classes DAO que Paulo Sérgio fez, com algumas correções

---
+++ src/barestoque/DAO/FornecedorDAO.java
@@ -0,0 +1,29 @@
+package barestoque.DAO;
+
+import barestoque.model.Fornecedor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class FornecedorDAO {
+    public FabricaConexao fabrica;
+    
+    public FornecedorDAO(){
+        fabrica = new FabricaConexao();
+    }
+    
+    public void inserirFornecedor(Fornecedor fornecedor){
+        String script = "Insert into fornecedor (nome, telefone, cnpj) values (?, ?, ?);";
+        try (Connection conexao = fabrica.conectar()){
+            
+            PreparedStatement declaracao = conexao.prepareStatement(script);
+            declaracao.setString(1, fornecedor.getNome());
+            declaracao.setString(2, fornecedor.getTelefone());
+            declaracao.setString(3, fornecedor.getCnpj());
+            declaracao.execute();
+        } catch (Exception e) {
+            System.err.println("Erro: "+e.getMessage());
+        }
+        
+    }
+    
+}

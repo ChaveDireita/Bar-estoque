b127625ab6f1d90a460479ea313c388a5c8a1072
Paulo Sérgio <47671822+psergio161@users.noreply.github.com>
2019-06-08 17:17:18 -0300

    Add files via upload

---
+++ src/barestoque/DAO/VendaDAO.java
@@ -0,0 +1,28 @@
+package barestoque.DAO;
+
+import barestoque.model.Venda;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class VendaDAO {
+    public FabricaConexao fabrica;
+    
+    public VendaDAO(){
+        fabrica = new FabricaConexao();
+    }
+    
+    public void inserirProduto(Venda venda){
+        String script = "Insert into venda (cliente, pratos, valor) values (?, ?, ?);";
+        try (Connection conexao = fabrica.conectar()){
+            
+            PreparedStatement declaracao = conexao.prepareStatement(script);
+            declaracao.setCliente(1, venda.getCliente());
+            declaracao.setArray(2, venda.getPratos());
+            declaracao.setDouble(3, venda.getValor());
+            declaracao.execute();
+        } catch (Exception e){
+                System.err.println("Erro: "+e.getMessage());
+        }
+    }
+}
+

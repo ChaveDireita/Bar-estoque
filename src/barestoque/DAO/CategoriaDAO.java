6cdee5777a21e56632a1173eb560bebb6b6a65da
DgK <47671653+DgK4711@users.noreply.github.com>
2019-06-08 15:06:46 -0300

    Novas classes DAO

---
+++ src/barestoque/DAO/CategoriaDAO.java
@@ -0,0 +1,26 @@
+package barestoque.DAO;
+
+import barestoque.model.Categoria;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class CategoriaDAO {
+    public FabricaConexao fabrica;
+    
+    public CategoriaDAO(){
+        fabrica = new FabricaConexao();
+    }
+    
+    public void inserirCategoria(Categoria categoria){
+        String script = "Insert into categoria (nome) values(?)";
+        try(Connection conexao = fabrica.conectar()){
+            
+            PreparedStatement declaracao = conexao.prepareStatement(script);
+            declaracao.setString(1, categoria.getNome());
+            declaracao.execute();
+        }catch(Exception e){
+            System.err.println("Erro: "+e.getMessage());
+        }
+        
+    }
+}
--- src/barestoque/Principal.java
+++ src/barestoque/Principal.java
@@ -1,9 +1,20 @@
+
 package barestoque;
+
+import barestoque.view.InicializadorLookAndFeel;
+import barestoque.view.Janela;
+import barestoque.view.telas.TelaInicial;
+import barestoque.view.telas.TelaMenu;
 
 public class Principal
 {
     public static void main(String[] args) 
     {
+        new InicializadorLookAndFeel ().init();
+        Janela j = new Janela ("Teste", 1280, 768, true);
+        j.aparecer();
+        j.setCena(new TelaInicial(j));
+        
         
     }
 }
